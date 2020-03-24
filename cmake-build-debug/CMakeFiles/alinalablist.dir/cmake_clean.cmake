cd2775d27c0e50303d77d0b0417565866c01e043
Lnntnv <55584949+Lnntnv@users.noreply.github.com>
2020-03-24 09:45:32 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/alinalablist.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/alinalablist.dir/LinkedList.cpp.o"
+  "CMakeFiles/alinalablist.dir/main.cpp.o"
+  "alinalablist"
+  "alinalablist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/alinalablist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
